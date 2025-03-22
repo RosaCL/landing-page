8104aa9e0c2a4d938be8652a5c9bdadfc95448be
Rosa <costurezati@gmail.com>
2025-03-22 12:46:30 -0300

    .

--- carnaval/script.js
+++ carnaval/script.js
@@ -1,4 +1,19 @@
-$(".toggle").click(function(){
-    $(this).toggleClass('on');
-    $(".resize").toggleClass("active");
-})
+header=document.querySelector('.header');
+navbar=document.querySelector('.header .navbar');
+menuBtn=document.querySelector('#menu-btn');
+
+menuBtn.onclick=()=>{
+    menuBtn.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll=()=>{
+    menuBtn.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+
+    if(window.scrollY>0){
+        header.classList.add('active');
+    }else{
+        header.classList.remove('active');
+    }
+}
